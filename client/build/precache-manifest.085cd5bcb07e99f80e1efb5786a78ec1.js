ae37f7b5f42a5cb1450dd09e70bbd04a76f25d23
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-15 12:02:14 -0700

    added a build folder

---
+++ client/build/precache-manifest.085cd5bcb07e99f80e1efb5786a78ec1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d1074d35bacb53755a4f00625f2961",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98af6dce0f5714341368",
+    "url": "/static/css/main.b5d134e9.chunk.css"
+  },
+  {
+    "revision": "14270d80bf28422628ec",
+    "url": "/static/js/2.f4d66932.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.f4d66932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98af6dce0f5714341368",
+    "url": "/static/js/main.4228125a.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
