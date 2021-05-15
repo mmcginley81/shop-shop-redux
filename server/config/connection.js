009a95bb30dd8e8ed8e4061df873d8be4b834eb8
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-15 11:33:22 -0700

    about to deploy at 11:33

--- server/config/connection.js
+++ server/config/connection.js
@@ -9,6 +9,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', 
 
 module.exports = mongoose.connection;
 
+// adding a comment to push again.
 
 // "mongodb+srv://mmcginley:Yougotthis21@cluster1.mkzfu.mongodb.net/mernshopping?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
 
