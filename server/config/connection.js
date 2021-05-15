75d82e8c6357fa68dd0400843404bb3632bbd212
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-15 11:13:18 -0700

    added dotenv

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping',  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -8,3 +8,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', 
 });
 
 module.exports = mongoose.connection;
+
+
+// "mongodb+srv://mmcginley:Yougotthis21@cluster1.mkzfu.mongodb.net/mernshopping?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+
+// @cluster1.mkzfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
