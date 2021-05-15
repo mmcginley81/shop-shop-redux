853cffa0c1a600f73d9f37e3efc2ed6c94a1732e
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-14 20:47:23 -0700

    seeded models

--- server/server.js
+++ server/server.js
@@ -37,17 +37,4 @@ db.once('open', () => {
   });
 });
 
-// Set your secret key. Remember to switch to your live secret key in production!
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc', {apiVersion: ''});
 
-// const session = await stripe.checkout.sessions.create({
-//   payment_method_types: ['card'],
-//   line_items: [{
-//     price: '{{PRICE_ID}}',
-//     quantity: 1,
-//   }],
-//   mode: 'payment',
-//   success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-//   cancel_url: 'https://example.com/cancel',
-// });
